c1d26f49c58074b94260aae9aa77108f220e01c4
Iryna Brusentsova <bio200480@gmail.com>
2023-02-28 15:41:35 +0200

    Add file to project

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,59 @@
+import styles from "./ColumnForm.module.scss";
+import { useState } from 'react';
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+
+
+
+const ColumnForm = (props) => {
+
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+    };
+
+	return (
+   <form onSubmit={handleSubmit}>
+        <span>Title:</span><TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        <span>Icon:</span><TextInput type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+        <Button>Add column</Button>
+</form>
+	);
+};
+
+export default ColumnForm;
+
+// ======
+
+// import styles from "./ColumnForm.module.scss";
+// import { useState } from 'react';
+// import Button from "../Button/Button";
+// import TextInput from "../TextInput/TextInput";
+
+// const ColumnForm = props => {
+
+
+//     const [value, setValue] = useState('');
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         props.action({ title: value });
+//         setValue('');
+//     };
+
+// 	return (
+//         <form className={styles.columnForm} onSubmit={handleSubmit}>
+//             {/* <input type="text" value={value} onChange={e => setValue(e.target.value)} /> */}
+//             {/* <input type="text" value={value} onChange={e => setValue(e.target.value)} /> */}  
+//            <span>Title: </span> <TextInput  type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+//            <span>Icon: </span> <TextInput   type="text"  value={value} onChange={(e) => setValue(e.target.value)}/>
+//             <Button>Add column</Button>
+//         </form>
+// 	);
+// };
+
+// export default ColumnForm;
