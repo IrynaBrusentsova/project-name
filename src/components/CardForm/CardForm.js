e86852e696b9f9e21651b6a765976076e2d1b13e
Iryna Brusentsova <bio200480@gmail.com>
2023-03-01 19:06:00 +0200

    Add Card to Column

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -7,17 +7,17 @@ const CardForm = props => {
     const [title, setTitle] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title });
+        props.action({ title: title }, props.columnId);
         setTitle('');
     };
 
-	return (
+    return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
             <Button>Add card</Button>
-           
+
         </form>
-	);
+    );
 };
 
 export default CardForm
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,19 @@
 import styles from './Column.module.scss';
-// import Card from '../Card/Card';
+import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 
 
 
 
 const Column = props => {
-   
+
     return (
         <article className={styles.column}>
-            <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2> 
+            <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <li key={card.id}>{card.title}</li>)}
-            </ul>       
-            <CardForm columnId={props.id} action={props.action}/>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                <CardForm columnId={props.id} action={props.action} />
+            </ul>
         </article>
     );
 
